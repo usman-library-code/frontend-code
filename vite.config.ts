ca7c52beda2814b6c678b3178ad994d9443f3efb
usman-library-code <usmanuar14@gmail.com>
2025-07-23 20:28:08 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,7 @@ export default defineConfig({
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
+         // await import("@replit/vite-plugin-cartographer").then((m) => ...);
           ),
         ]
       : []),
